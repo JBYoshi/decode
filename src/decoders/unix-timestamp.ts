c087f2707c5e7710322bf35e2ce1585dd6905d7e
JBYoshi <12983479+JBYoshi@users.noreply.github.com>
2024-01-08 13:36:46 -0600

    Re-allow decoding timestamps from strings.

--- src/decoders/unix-timestamp.ts
+++ src/decoders/unix-timestamp.ts
@@ -1,13 +1,14 @@
 import { DateNode } from "../nodes/date";
 import { FormatNode } from "../nodes/format";
 import { NumberNode } from "../nodes/number";
+import { StringNode } from "../nodes/string";
 import { DecodeNode } from "../types";
 
 // This time in seconds is the year 2969. I doubt it'll be a problem.
 const SECONDS_MILLISECONDS_CUTOFF = new Date("Jan 1, 1971, 00:00:00 UTC").getTime();
 
 export function decodeUnixTimestamp(input: DecodeNode): DecodeNode | null {
-    if (input instanceof NumberNode) {
+    if (input instanceof NumberNode || (input instanceof StringNode && input.value.match(/^[1-9][0-9]*(\.[0-9]+)?$/))) {
         let timestamp = Number(input.value);
         // Could be a date.
         // For compactness, let's pick whatever's closer to now.
