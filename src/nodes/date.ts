41d77fd025d7b10d61dd921527de03d78f7afff4
JBYoshi <12983479+JBYoshi@users.noreply.github.com>
2024-01-09 14:25:45 -0600

    Change date text displays to show full information.

--- src/nodes/date.ts
+++ src/nodes/date.ts
@@ -14,8 +14,8 @@ export class DateNode extends DecodeNode {
 
     get defaultRepresentations(): Representation[] {
         return [
-            {format: "Local date", value: this.value.toLocaleString()},
-            {format: "UTC string", value: this.value.toUTCString()},
+            {format: "Local", value: this.value.toLocaleString(undefined, {dateStyle: "full", timeStyle: "full"})},
+            {format: "UTC", value: this.value.toLocaleString(undefined, {timeZone: "UTC", dateStyle: "full", timeStyle: "full"})},
             {format: "ISO string", value: this.value.toISOString()},
             {format: "UTC offset (milliseconds)", value: this.value.getTime() + ""},
             {format: "UTC offset (seconds)", value: this.value.getTime() / 1000 + ""}
