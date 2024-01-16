edd0678aff0426f7df10a623f9a81c2b6b1b28e4
JBYoshi <12983479+JBYoshi@users.noreply.github.com>
2024-01-16 16:35:36 -0600

    Re-add correct names to ASN.1.

--- src/decoders/asn1.ts
+++ src/decoders/asn1.ts
@@ -1,4 +1,4 @@
-import { AsnType, BaseStringBlock, BitString, Boolean, Choice, Constructed, EndOfContent, Integer, Null, ObjectIdentifier, OctetString, RelativeObjectIdentifier, fromBER } from "asn1js";
+import { AsnType, BaseStringBlock, BitString, BmpString, Boolean, CharacterString, Choice, Constructed, DATE, DateTime, Duration, EndOfContent, GeneralString, GeneralizedTime, GraphicString, IA5String, Integer, Null, NumericString, ObjectIdentifier, OctetString, PrintableString, RelativeObjectIdentifier, Sequence, TIME, TeletexString, TimeOfDay, UTCTime, UniversalString, Utf8String, VideotexString, VisibleString, fromBER } from "asn1js";
 import { BytesNode } from "../nodes/bytes";
 import { DecodeNode } from "../types";
 import { ConstantNode } from "../nodes/constant";
@@ -20,7 +20,13 @@ function toNode(data: AsnType): DecodeNode {
     } else if (data instanceof RelativeObjectIdentifier) {
         return new ConstantNode("Relative Object Identifier", data.getValue());
     } else if (data instanceof Constructed) {
-        return new ListNode(data.name,
+        let name = "";
+        if (data instanceof Sequence) {
+            name = "Sequence";
+        } else if (data instanceof Set) {
+            name = "Set";
+        }
+        return new ListNode(name,
             data.valueBlock.value.map(block => toNode(block))
         );
     } else if (data instanceof Choice) {
@@ -34,9 +40,49 @@ function toNode(data: AsnType): DecodeNode {
         // There's no convenient decode function so I'll use this as a workaround.
         return new BitsNode(data.toString("ascii").split(" : ")[1]);
     } else if (data instanceof BaseStringBlock) {
-        return new StringNode(data.getValue(), data.name);
+        let name = "";
+        if (data instanceof TIME) {
+            name = "Time";
+        } else if (data instanceof DATE) {
+            name = "Date";
+        } else if (data instanceof DateTime) {
+            name = "Date/Time";
+        } else if (data instanceof TimeOfDay) {
+            name = "Time of Day";
+        } else if (data instanceof Duration) {
+            name = "Duration";
+        } else if (data instanceof Utf8String) {
+            name = "UTF-8 String";
+        } else if (data instanceof NumericString) {
+            name = "Numeric String";
+        } else if (data instanceof PrintableString) {
+            name = "Printable String";
+        } else if (data instanceof TeletexString) {
+            name = "Teletex String";
+        } else if (data instanceof VideotexString) {
+            name = "Videotex String";
+        } else if (data instanceof IA5String) {
+            name = "IA5 String";
+        } else if (data instanceof GeneralizedTime) {
+            name = "Generalized Time";
+        } else if (data instanceof UTCTime) {
+            name = "UTC Time";
+        } else if (data instanceof GraphicString) {
+            name = "Graphic String";
+        } else if (data instanceof VisibleString) {
+            name = "Visible String";
+        } else if (data instanceof GeneralString) {
+            name = "General String";
+        } else if (data instanceof UniversalString) {
+            name = "Teletex String";
+        } else if (data instanceof CharacterString) {
+            name = "Character String";
+        } else if (data instanceof BmpString) {
+            name = "BMP String";
+        }
+        return new StringNode(data.getValue(), name);
     } else {
-        return new ConstantNode(data.name, "TODO");
+        return new ConstantNode("Unknown", "TODO");
     }
 }
 
